00f100c10e0f717f1f5811932beff0b12523c2c0
Galaco <mail@galaco.me>
2018-08-29 14:41:58 +0100

    Add BGR888 support

--- colourformat/datasize.go
+++ colourformat/datasize.go
@@ -2,30 +2,24 @@ package colourformat
 
 
 func GetImageSizeInBytes(index ColorFormat, width int, height int) int {
+	size := int(float32(width * height) * BytesPerPixel(index))
 	switch index {
-	case BGRX8888:
-		return int(float32(width * height) * BytesPerPixel(index))
-	case RGB888:
-		return int(float32(width * height) * BytesPerPixel(index))
 	case Dxt1:
-		size := int(float32(width * height) * BytesPerPixel(index))
 		if size < 8 {
 			size = 8
 		}
 		return size
-	case Dxt5:
-		return int(float32(width * height) * BytesPerPixel(index))
-	default:
-		return 0
 	}
 
-	return 0
+	return size
 }
 
 func BytesPerPixel(format ColorFormat) float32 {
 	switch format {
 	case BGRX8888:
 		return 4
+	case BGR888:
+		return 3
 	case RGB888:
 		return 3
 	case Dxt1:
