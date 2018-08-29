3a2cdd4b89ed3e8069eebc7eb8ee924b95bfbae8
Galaco <mail@galaco.me>
2018-08-29 14:49:57 +0100

    Provisional support for all formats
    Note: its up to whatever uses this library to handle the actual format

--- colourformat/datasize.go
+++ colourformat/datasize.go
@@ -16,16 +16,58 @@ func GetImageSizeInBytes(index ColorFormat, width int, height int) int {
 
 func BytesPerPixel(format ColorFormat) float32 {
 	switch format {
-	case BGRX8888:
+	case RGBA8888:
 		return 4
-	case BGR888:
-		return 3
+	case ABGR8888:
+		return 4
 	case RGB888:
 		return 3
+	case BGR888:
+		return 3
+	case RGB565:
+		return 2
+	case I8:
+		return 1
+	case IA88:
+		return 2
+	case P8:
+		return 1
+	case A8:
+		return 1
+	case RGB888_BLUESCREEN:
+		return 3
+	case BGR888_BLUESCREEN:
+		return 3
+	case ARGB8888:
+		return 4
+	case BGRA8888:
+		return 4
 	case Dxt1:
 		return 0.5
 	case Dxt5:
 		return 1
+	case BGRX8888:
+		return 4
+	case BGR565:
+		return 2
+	case BGRX5551:
+		return 2
+	case BGRA4444:
+		return 2
+	case Dxt1_OneBitAlpha:
+		return 0.5
+	case BGRA5551:
+		return 2
+	case UV88:
+		return 2
+	case UVWQ8888:
+		return 4
+	case RGBA16161616F:
+		return 8
+	case RGBA16161616:
+		return 8
+	case UVLX8888:
+		return 4
 	}
 
 	return 0
